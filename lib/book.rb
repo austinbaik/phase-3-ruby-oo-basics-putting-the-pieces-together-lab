3597c9aeb153a13f6a218da8a610c6b793e01688
Austin <baik.austin@gmail.com>
2022-07-16 18:50:02 -0700

    commit tree 7a963ad75351fb721ad9f7a1d4d202aa023d5cf7

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
 
 
+class Book 
+    attr_reader :title
+
+    def initialize(title) 
+        @title = title 
+        # @page_count = page_count 
+    end
+
+    attr_reader :author
+    def author=(author)
+        @author = author
+    end
+
+    attr_reader :page_count 
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    attr_reader :genre 
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+ 
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,41 @@
 # Make your shoe class here!
+
+class Shoe 
+    attr_accessor :brand
+
+    def initialize (brand)
+        @brand= brand 
+    end 
+
+    attr_reader :color
+
+    def color=(color)
+        @color=color
+    end 
+
+    attr_reader :size 
+
+     def size=(size)
+        @size=size
+     end
+
+     attr_reader :material
+     
+     def material=(material)
+        @material = material
+     end
+
+     attr_reader :condition
+    
+     def condition=(condition)
+        @condition=condition
+     end
+
+     def cobble
+        puts "Your shoe is as good as new!"
+        @condition="new"
+     end
+
+ 
+
+end 
